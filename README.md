8c0fbd30efa42cd2f57cc117b97721f07ecc72e5
Rohan C. Shah <rcs353@cornell.edu>
2025-03-26 19:16:18 -0400

    initial frontend commit + README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cs6386-final-proj
+# Modernized Recipe Search Engine
+
+Run <code>npm install</code> and <code>npm install axios</code> in the <code>frontend</code> directory.
+To launch frontend, run <code>npm run dev</code>import React, { useState } from "react";
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RecipeSearch from "./components/RecipeSearch.jsx";
+
+function App() {
+  return (
+    <div style={{ maxWidth: "600px", margin: "0 auto" }}>
+      <h1>Recipe Search</h1>
+      <RecipeSearch />
+    </div>
+  );
+}
+
+export default App;
